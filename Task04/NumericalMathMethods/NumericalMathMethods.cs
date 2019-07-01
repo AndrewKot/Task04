caab7118571fa5bae845294643599c517cee8d2a
AndrewKotM <andrew.m.kot@gmail.com>
2019-07-01 17:03:14 +0300

    Day01

--- Task04/NumericalMathMethods/NumericalMathMethods.cs
+++ Task04/NumericalMathMethods/NumericalMathMethods.cs
@@ -8,6 +8,33 @@ namespace NumericalMathMethods
 {
     public static class FindRootOfNumber
     {
+        private static bool Resolution(double value, double accuracy)
+        {
+            if (value >= 0 && accuracy >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
+        public static double Root(double value, int degree, double accuracy, double initialValue)
+        {
+            if (Resolution(value, accuracy))
+            {
+               
+                //while (true)
+                //{
+
+                //}
+                return 0.0d;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
     }
 }
